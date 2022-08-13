7c5236775996f1c866790c1272ac425d6a2606e9
zhaoyake <13083600925@139.com>
2022-08-13 16:41:46 +0800

    add public path

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,4 @@
-module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/circle-demo' : '/'
-}
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  transpileDependencies: true
+})
